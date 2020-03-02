04df79b74ea8ab1a67bfcd551f97455854dcdee3
ThomasHuru <55877178+ThomasHuru@users.noreply.github.com>
2020-03-02 12:59:36 +0100

    Update CalculatorResourceTest.java
    added testMultiplication() and testDivision()

--- devops-workshop-master/src/test/java/CalculatorResourceTest.java
+++ devops-workshop-master/src/test/java/CalculatorResourceTest.java
@@ -37,4 +37,21 @@ public class CalculatorResourceTest{
         expression = "20-2";
         assertEquals(200, calculatorResource.subtraction(expression));
     }
+    public void testMultiplication(){
+        CalculatorResource calculatorResource = new CalculatorResource();
+
+        String expression = "133*10";
+        assertEquals(1330, calculatorResource.Multiplication(expression));
+
+        expression = "3*21";
+        assertEquals(63, calculatorResource.Multiplication(expression));
+    }
+    public void testDivision(){
+         CalculatorResource calculatorResource = new CalculatorResource();
+
+        String expression = "1330/10";
+        assertEquals(133, calculatorResource.Multiplication(expression));
+
+        expression = "10/3";
+        assertEquals(3.33, calculatorResource.Multiplication(expression));
 }
